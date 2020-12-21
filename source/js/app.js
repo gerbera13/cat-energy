bbb128c6e1033d0db17a120093b7f0fa14a1fa7f
Oksana <gerbera_13@mail.ru>
2020-12-21 19:31:54 +0300

    верстка адаптивной сетки главной страницы

---
+++ source/js/app.js
@@ -0,0 +1,11 @@
+
+  const first = document.querySelector(".first");
+  const second = document.querySelector(".second");
+  const slider = document.querySelector("#slider");
+
+  slider.addEventListener("input", evt => {
+    const value = evt.target.valueAsNumber;
+    first.style.width = `${value}%`;
+    second.style.width = `${100 - value}%`;
+  });
+
