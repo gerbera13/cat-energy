f0995376338ed0574a83689ed2d42e7f0817b66c
Oksana <gerbera_13@mail.ru>
2021-01-21 10:41:27 +0300

    верстка адаптивной сетки главной страницы index.html

--- source/js/app.js
+++ source/js/app.js
@@ -2,8 +2,22 @@ const first = document.querySelector(".first");
 const second = document.querySelector(".second");
 const slider = document.querySelector("#slider");
 
-slider.addEventListener("input", evt => {
-  const value = evt.target.valueAsNumber;
-  first.style.width = `${value}%`;
-  second.style.width = `${100 - value}%`;
-});
+if (slider) {
+  slider.addEventListener("input", evt => {
+    const value = evt.target.valueAsNumber;
+    first.style.width = `${100 - value}%`;
+    second.style.width = `${value}%`;
+  });
+}
+
+const burger = document.querySelector(".js-button");
+const menu = document.querySelector(".js-menu");
+
+if (burger) {
+  burger.classList.remove('opened');
+  menu.classList.remove('opened');
+  burger.addEventListener('click', function (evt) {
+    burger.classList.toggle('opened');
+    menu.classList.toggle('opened');
+  })
+}
